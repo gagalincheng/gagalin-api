81c0bb69dcada3f6130d6ada6996a1f69d640cbb
galinzheng <galinzheng@tencent.com>
2020-09-20 17:31:10 +0800

    upload return cdnPath

--- controller/upload.js
+++ controller/upload.js
@@ -2,20 +2,23 @@ const path = require('path');
 const fs = require('fs');
 
 const main = async function(ctx) {
+  const host = ctx.headers.host;
   const imgDir = 'static/photos/';
   const filePaths = [];
   const files = ctx.request.files || {};
   for (let key in files) {
     const file = files[key];
-    // const writeName = new Date().getTime();
+    const ext = file.name.substr(file.name.indexOf('.'));
+    const writeName = new Date().getTime() + ext;
 
-    const filePath = path.join(__dirname, `../${imgDir}${file.name}`);
+    const filePath = path.join(__dirname, `../${imgDir}${writeName}`);
     // const reader = fs.createReadStream(file.path);
     // const writer = fs.createWriteStream(filePath);
     // reader.pipe(writer);
     const imgBuffer = fs.readFileSync(file.path)
     fs.writeFileSync(filePath, imgBuffer)
-    filePaths.push(filePath);
+    const cdnPath = `http://${host}/photos/${writeName}`;
+    filePaths.push(cdnPath);
   }
 
   ctx.body = filePaths;
